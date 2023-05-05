8f9504cff774947dfdf53a939b6cc52f223c3669
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-05 09:23:51 +0100

    made the navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,28 @@
 import "./NavBar.css"
 import  Logo from "../../images/logo.png"
+import { useState } from "react"
 
 function NavBar() {
+
+    const [showNav, setShowNav] = useState(false)
+    // const [showBurger, setShowBurger] = useState(false)
     return (
         <nav className = "nav-bar">
         <img src= {Logo} alt="" id= "logo"/>
-        <div className="nav-links">
+        <div className={`nav-links ${!showNav && "hide"}`} >
+            <p className = "close" onClick ={() => setShowNav(false)}>X</p>
             <p>Home</p>
             <p>Categories</p>
             <p>Products</p>
             <p>About us</p>
             <p>Pricing</p>
             <button>Join us</button>
+        </div>
+
+        <div className="burger-nav" onClick = {() => setShowNav(true)}>
+            <div className="burger1"></div>
+            <div className="burger1"></div>
+            <div className="burger1"></div>
         </div>
     </nav>
     )

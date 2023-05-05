55491a8409c50b1f6426da674f3b49d047152a52
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-05 04:01:21 +0100

    Working on the BMI Calculator Page

--- src/components/BMICalculator/BMICalculator.jsx
+++ src/components/BMICalculator/BMICalculator.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import NavBar from "../NavBar/NavBar";
+import Person from "../../images/person.png"
+import "./BMICalculator.css"
+import { Link } from "react-router-dom";
 
 function BMICalculator() {
 
@@ -15,49 +18,54 @@ function BMICalculator() {
             </div>
             
 
-            <h1>BMI (Body Mass Index) Calculator</h1>
+            <h1 id= "bmiCalc-title">BMI (Body Mass Index) Calculator</h1>
 
-            <div className="img">
+            <div className="calculator">
+                <img src= {Person} alt="" id= "personImg"/>
+                
 
+                <form>
+                    <div className="input-group">
+                        <label>Select Gender</label>
+                        <select name="" id="" className = "main-input">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+
+                    <div className="input-group">
+                        <label>Age</label>
+                        <select name="" id="" className = "main-input">
+                            <option value="1">1</option>
+                        </select>
+                    </div>
+
+                    <div className="input-group">
+                        <label>Weight</label>
+                        <select name="" id="" className = "main-input">
+                            <option value="1">1</option>
+                        </select>
+                        <select name="" id="">
+                            <option value="kg">kg</option>
+                        </select>
+                    </div>
+
+                    <div className="input-group">
+                        <label>Height</label>
+                        <select name="" id="" className = "main-input">
+                            <option value="1">1</option>
+                        </select>
+                        <select name="" id="">
+                            <option value="kg">cm</option>
+                        </select>
+                    </div>
+                    
+                    <Link to = "/BMI-info" className = "bmiCalc-btn">Calculate BMI</Link>
+                </form>
+                                
             </div>
+
             
-            <form>
-                <div className="imput-group">
-                    <label>Select Gender</label>
-                    <select name="" id="">
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </select>
-                </div>
-
-                <div className="input-group">
-                    <label>Age</label>
-                    <select name="" id="">
-                        <option value="1">1</option>
-                    </select>
-                </div>
-
-                <div className="input-group">
-                    <label>Weight</label>
-                    <select name="" id="">
-                        <option value="1">1</option>
-                    </select>
-                    <select name="" id="">
-                        <option value="kg">kg</option>
-                    </select>
-                </div>
-
-                <div className="input-group">
-                    <label>Height</label>
-                    <select name="" id="">
-                        <option value="1">1</option>
-                    </select>
-                    <select name="" id="">
-                        <option value="kg">cm</option>
-                    </select>
-                </div>
-
-            </form>
         </div>
     )
 }

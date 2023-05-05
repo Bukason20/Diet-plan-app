2c3ce090471e197606ad5937a22de233129969ef
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-05 17:01:01 +0100

    added contents for underweight

--- src/Weight Ranges/UnderWeight.jsx
+++ src/Weight Ranges/UnderWeight.jsx
@@ -29,6 +29,34 @@ function UnderWeight() {
                         </div>
                     </div>
                 </div>
+
+                <div className="recipe">
+                    <p className= "recipe-name">2) Chicken and Sweet Potato Bowl</p>
+                    <div className="recipe-procedures">
+                        <div className="ingredients">
+                            <p>Ingredients:</p>
+                            <ul>
+                                <li>1 sweet potato, peeled and diced</li>
+                                <li>1 boneless, skinless chicken breast, cut into small pieces</li>
+                                <li>1 tbsp olive oil</li>
+                                <li>1/2 tsp paprika</li>
+                                <li>Salt and pepper, to taste</li>
+                                <li>1/4 cup chopped fresh parsley</li>
+                            </ul>
+                        </div>
+
+                        <div className="instructions">
+                            <p>Instructions:</p>
+                            <ol>
+                                <li>Preheat the oven to 400°F (200°C).</li>
+                                <li>Toss the sweet potato with olive oil, paprika, salt, and pepper. Spread the sweet potato in a single layer on a baking sheet.</li>
+                                <li>Roast the sweet potato for 20-25 minutes or until tender and lightly browned.</li>
+                                <li>Meanwhile, heat a pan over medium-high heat. Add the chicken and cook until browned and cooked through.</li>
+                                <li>Combine the chicken and sweet potato in a bowl. Top with chopped parsley and serve.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )

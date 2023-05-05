071002d3134503f9cf0b03780ff85591dccdcc55
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-05 02:12:13 +0100

    Added routes to the file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import BMICalculator from "./components/BMICalculator/BMICalculator"
 import BmiInfo from "./components/BmiInfo/BmiInfo"
 import Homepage from "./components/HomePage/Homepage"
 import "./index.css"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div>
-    <Homepage />
-    {/* <BmiInfo /> */}
-    {/* <BMICalculator /> */}
-    </div>
+    <Router>
+      <div className = "app-container">
+        <Switch>
+          <Route exact path = "/">
+            <Homepage />
+          </Route>
+
+          <Route path = "/BMI-calculator">
+            <BMICalculator />
+          </Route>
+
+          <Route path = "/BMI-info">
+            <BmiInfo />
+          </Route>
+        </Switch>
+        
+      </div>
+
+    </Router>
+    
   )
 }
 
